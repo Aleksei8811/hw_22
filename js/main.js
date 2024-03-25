48a6f86e7127c5e1cf46e86eec754187224c3523
Aleksei <alex.letters88@mail.ru>
2024-03-25 08:46:04 +0600

    Сделал две задачи по JS

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//Задача №1
+
+// let askName = prompt('Введите ваше имя');
+// let askAge = prompt('Введите ваш возраст');
+// let result = `Привет, меня зовут ${askName}, мне ${askAge} лет!`;
+// alert(result);
+
+//Задача №2
+
+// let askNum_1 = parseInt(prompt('Введите первое число'));
+// let askNum_2 = parseInt(prompt('Введите второе число'));
+
+// let result_1 = askNum_1 + askNum_2;
+// let result_2 = askNum_1 - askNum_2;
+// let result_3 = askNum_1 * askNum_2;
+// let result_4 = askNum_1 / askNum_2;
+// alert(`${result_1}, ${result_2}, ${result_3}, ${result_4}`);
+
+// alert(`${askNum_1 + askNum_2}, ${askNum_1 - askNum_2}, ${askNum_1 * askNum_2}, ${askNum_1 / askNum_2}`);
+
+//Задача №3
+
