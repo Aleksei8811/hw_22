7b91befb3a29be46c6a7e68b86fe1cedb56d93ea
Aleksei <alex.letters88@mail.ru>
2024-03-25 09:17:53 +0600

    Сделал четвертую задачу

--- js/main.js
+++ js/main.js
@@ -20,15 +20,29 @@
 
 //Задача №3
 
-let askAge = prompt('Укажите ваш возраст');
-if (askAge >= 6 && askAge <= 10) {
- console.log('Идем на детскую площадку');
-} else if (askAge >= 11 && askAge <= 16) {
-    console.log("Едим в горы");
-} else if (askAge >= 17 && askAge <= 20) {
-    console.log("Идем в кафе");
-} else if (askAge >= 21 && askAge <= 25) {
-    console.log("Идем на дискотеку");
-} else {
-    console.log('Неверный возраст. Сидим дома.');
-}
+// let askAge = prompt('Укажите ваш возраст');
+// if (askAge >= 6 && askAge <= 10) {
+//  console.log('Идем на детскую площадку');
+// } else if (askAge >= 11 && askAge <= 16) {
+//     console.log("Едим в горы");
+// } else if (askAge >= 17 && askAge <= 20) {
+//     console.log("Идем в кафе");
+// } else if (askAge >= 21 && askAge <= 25) {
+//     console.log("Идем на дискотеку");
+// } else {
+//     console.log('Неверный возраст. Сидим дома.');
+// }
+
+//Задача №4
+
+let askNumber = parseInt(prompt('МИНОМЕТ. Укажите число'));
+
+if (askNumber > 28 && askNumber < 30) {
+        console.log('ПОПАЛ');
+} else if (askNumber >= 30) {
+       console.log('ПЕРЕЛЕТ');
+} else if (askNumber > 0 && askNumber <= 28) {
+       console.log('НЕДОЛЕТ');
+} else if (askNumber <= 0) {
+    console.log('НЕ БЕЙ ПО СВОИМ');
+} 
